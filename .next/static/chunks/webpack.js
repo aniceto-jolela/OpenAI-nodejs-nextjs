936102d9b33ca44d87f2b7f7617f122fe6c101d1
aniceto-jolela <anicetojosejolelaj@gmail.com>
2023-03-08 15:18:32 +0100

    add update thr file index.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e5a53dad66f8d2fe"; }
+/******/ 		__webpack_require__.h = function() { return "b99919f40c84eec4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.20d29de7c1f39b76.hot-update.js
+++ .next/static/webpack/webpack.20d29de7c1f39b76.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "2de3d496cac792aa"; }
+/******/ 	__webpack_require__.h = function() { return "0588769d0a1b8e50"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
